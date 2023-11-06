472d492c8052b0fd425bb7f5249da2877cd3e7cd
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-11-06 16:35:59 +0600

    Implement POST endpoint for creating bookings

--- index.js
+++ index.js
@@ -1,11 +1,84 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.USER_NAME)
+console.log(process.env.USER_PASS)
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.7auoehb.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    const bookingCollection=client.db("HotelBooking").collection("Bookings")
+    const addbookingCollection=client.db("HotelBooking").collection("books")
+    // await client.connect();
+    
+    app.get('/Bookings',async(req,res)=>{
+      const cursor=bookingCollection.find();
+      const result=await cursor.toArray()
+      res.send(result)
+      // res.send({
+      //   total:result.length,result
+      //  })
+    })
+
+    app.get('/Bookings/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const options = {
+      
+        projection: {roomDescription: 1, price: 1,roomSize:1,availability:1,roomImages:1 },
+      };
+      const result=await bookingCollection.findOne(query,options)
+      res.send(result)
+  })
+
+  app.get('/books',async(req,res)=>{
+    const cursor=addbookingCollection.find();
+    const result=await cursor.toArray()
+    res.send(result)
+  })
+
+  app.post('/books',async(req,res)=>{
+    const booking=req.body;
+   console.log(booking)
+   const result=await addbookingCollection.insertOne(booking)
+   res.send(result)
+})
+
+app.delete('/books/:id',async(req,res)=>{
+  const id=req.params.id;
+  console.log(id)
+  const query={_id: id}
+  const result=await addbookingCollection.deleteOne(query)
+  res.send(result)
+})
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,3 +87,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
